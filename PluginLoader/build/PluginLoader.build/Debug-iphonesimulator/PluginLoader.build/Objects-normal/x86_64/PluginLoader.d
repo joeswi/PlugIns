61195152c38ae988cee330b24d88f216469f59ae
joeswi <joeswi@gmail.com>
2017-02-13 12:26:54 +0800

    dead code = no

--- PluginLoader/build/PluginLoader.build/Debug-iphonesimulator/PluginLoader.build/Objects-normal/x86_64/PluginLoader.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/xxw/Documents/workspace/PlugIns/PluginLoader/PluginLoader/PluginLoader.m \
-  /Users/xxw/Documents/workspace/PlugIns/PluginLoader/PluginLoader/PluginLoader.h
